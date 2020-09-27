d063b79654d7582efbdbc03719573da3f9c07a14
Liam Idrovo <lai3@duke.edu>
2020-09-26 21:40:13 -0400

    Original message is 42

--- src/com/hw4/Main.java
+++ src/com/hw4/Main.java
@@ -1,8 +1,14 @@
 package com.hw4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws FileNotFoundException {
+        String[] decryptionStrings = RSA.getDecryptionStrings("resources/secretMessage.txt", "resources/publicKey.txt", "resources/privateKey.txt");
+        String originalMessage = RSA.decrypt(decryptionStrings[0], decryptionStrings[2], decryptionStrings[1]);
+        System.out.println("Original message:\t" + originalMessage);
     }
 }
